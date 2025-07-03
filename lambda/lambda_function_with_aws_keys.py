ed83172d5b883e65afb88811ba22804db3f6bde0
lalohike <lalohike@redhat.com>
2025-07-03 08:13:51 -0500

    Update lambda_function_with_aws_keys.py
    Removed part of lambda function that prints aws keys, session token, 'body' section of the return.

--- lambda/lambda_function_with_aws_keys.py
+++ lambda/lambda_function_with_aws_keys.py
@@ -1,11 +1,7 @@
-# This lambda function will not only export the S3 Key of the uploaded file,
-# but it will also export the current aws access key and secret key in a secure manner. 
-
 import json
 import requests
 import boto3
 from botocore.exceptions import ClientError
-
 
 def get_secret():
     secret_name = "aap_api_key"
@@ -33,7 +29,7 @@ def lambda_handler(event, context):
     key = event['Records'][0]['s3']['object']['key']
     api_secret = get_secret()
 
-    print(f"Triggering AAP job via API launch for key: {key}")
+    print(f"Triggering AAP job via API launch for s3_key: {key}")
 
     # Get temporary AWS credentials from Lambda's execution role
     credentials = boto3.Session().get_credentials().get_frozen_credentials()
@@ -64,13 +60,8 @@ def lambda_handler(event, context):
 
     if response.status_code in [200, 201, 202]:
         print("✅ AAP job triggered successfully!")
-        print(json.dumps(payload, indent=2))
-        print(aws_access_key)
-        print(aws_secret_key)
     else:
         print(f"❌ Failed to trigger AAP: {response.status_code} - {response.text}")
-
     return {
-        'statusCode': response.status_code,
-        'body': response.text
+        'statusCode': response.status_code
     }
